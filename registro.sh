c4706b4028f75be78916fa3aecac5364524f58a7
lester12d <102380033+lester12d@users.noreply.github.com>
2022-03-25 11:46:53 -0600

    Add files via upload
    program bash dates de usurious

---
+++ registro.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "nombre" 
+read name 
+echo "apellidos"
+read apellidos
+echo "telefono"
+read telefono
+echo "email"
+read email
+echo "edad"
+read edad
+echo "cumpleaños"
+read cumple
+echo "bienvenido(a) $nombre $apellidos deacuerdo a tus $edad aun asi que tu fecha de nacimiento es el $cumple aun cubres con la edad requerida, asi que te avisaremos cuando  empezaras a trabajar ya sea en tu correo ques el $email o tu telefono que el $telefono que nos has prporcionado"
